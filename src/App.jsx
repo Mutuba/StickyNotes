abdc937d8b0f1b768b1a48b2becdd703c2bbe599
Mutuba <danielmutubait@gmail.com>
2024-08-22 19:02:52 +0300

    Add appwrite configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import NotesPage from "./pages/NotesPage";
-import NoteCards from "./components/NoteCards";
 
 const App = () => {
   return (
     <div id="app">
       <NotesPage />
-      <NoteCards />
     </div>
   );
 };
--- src/__tests__/pages/NotesPages.test.jsx
+++ src/__tests__/pages/NotesPages.test.jsx
@@ -1,11 +1,15 @@
 import { describe, test, expect, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { fakeData as notes } from "../../assets/fakeData";
 import NotesPage from "../../pages/NotesPage";
-describe("NotesPage test", () => {
+
+describe("NotesPage Component", () => {
   beforeEach(() => {
     render(<NotesPage />);
   });
   test("Should show title", () => {
-    expect(screen.getByText(/NotesPage/i)).toBeInTheDocument();
+    const cardText = screen.getAllByText(/Resources:/i);
+    expect(cardText[0]).toBeInTheDocument();
+    expect(screen.getAllByTestId("note-card")).toHaveLength(notes.length);
   });
 });
